dda637e94e5e27bd0ac0271249bb35135111b06d
Paris Vinuya <parisvinuya@Pariss-MacBook-Air.local>
2023-04-18 09:23:35 +1000

    Added about page

---
+++ main/src/pages/landing/Contact.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Contact: React.FC = () => {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <p>Our contact page</p>
+    </div>
+  );
+};
+
+export default Contact;
