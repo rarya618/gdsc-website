c169e7a24aad6feb2d0c54c4ab61758083de6fa3
Paris Vinuya <parisvinuya@Pariss-MacBook-Air.local>
2023-04-25 14:46:12 +1000

    added social media links

--- main/src/pages/landing/Contact.tsx
+++ main/src/pages/landing/Contact.tsx
@@ -1,11 +1,47 @@
 import React from "react";
 import contactcss from "./Contact.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faInstagram,
+  faFacebook,
+  faLinkedin,
+  faDiscord,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Contact: React.FC = () => {
   return (
     <div>
-      <div className={contactcss.background}>
-        <div className={contactcss.contact}>Contact</div>
+      <div>
+        <div className={contactcss.background}>
+          <div className={contactcss.contact}>Contact</div>
+        </div>
+      </div>
+
+      <div>
+        <h1>Follow our Socials</h1>
+        <div>
+          <a href="https://www.instagram.com/gdscusyd/">
+            <FontAwesomeIcon
+              className={contactcss.instagram}
+              icon={faInstagram}
+            />
+          </a>
+          <a href="https://www.facebook.com/gdsc.usyd">
+            <FontAwesomeIcon
+              className={contactcss.facebook}
+              icon={faFacebook}
+            />
+          </a>
+          <a href="https://www.linkedin.com/company/gdsc-usyd/">
+            <FontAwesomeIcon
+              className={contactcss.linkedin}
+              icon={faLinkedin}
+            />
+          </a>
+          <a href="https://discord.com/channels/872033047652990986/872033047652990989">
+            <FontAwesomeIcon className={contactcss.discord} icon={faDiscord} />
+          </a>
+        </div>
       </div>
     </div>
   );
