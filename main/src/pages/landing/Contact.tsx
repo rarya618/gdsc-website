3bd93151782313cb48d6803c7a73bb392470db90
Paris Vinuya <parisvinuya@Pariss-MacBook-Air.local>
2023-04-29 16:35:11 +1000

    changed colour of social media links and added in text above social media icons

--- main/src/pages/landing/Contact.tsx
+++ main/src/pages/landing/Contact.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import contactcss from "./Contact.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -13,12 +14,16 @@ const Contact: React.FC = () => {
     <div>
       <div>
         <div className={contactcss.background}>
-          <div className={contactcss.contact}>Contact</div>
+          <div className={contactcss.contact}>Contact Us</div>
         </div>
       </div>
 
-      <div>
-        <h1>Follow our Socials</h1>
+      <div className={contactcss.whitebackground}>
+        <h1>Follow Our Socials</h1>
+        <p className={contactcss.socialmedia}>
+          Stay connected with GDSC USYD by following us on our social media
+          channels:
+        </p>
         <div>
           <a href="https://www.instagram.com/gdscusyd/">
             <FontAwesomeIcon
@@ -26,20 +31,23 @@ const Contact: React.FC = () => {
               icon={faInstagram}
             />
           </a>
+
           <a href="https://www.facebook.com/gdsc.usyd">
             <FontAwesomeIcon
               className={contactcss.facebook}
               icon={faFacebook}
             />
           </a>
+
+          <a href="https://discord.com/channels/872033047652990986/872033047652990989">
+            <FontAwesomeIcon className={contactcss.discord} icon={faDiscord} />
+          </a>
+
           <a href="https://www.linkedin.com/company/gdsc-usyd/">
             <FontAwesomeIcon
               className={contactcss.linkedin}
               icon={faLinkedin}
             />
-          </a>
-          <a href="https://discord.com/channels/872033047652990986/872033047652990989">
-            <FontAwesomeIcon className={contactcss.discord} icon={faDiscord} />
           </a>
         </div>
       </div>
