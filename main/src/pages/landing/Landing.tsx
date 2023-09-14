06c3815bd62a19b69d5f620c5fcd23e4b81a19c7
Pratul Singh Raghava <pratulsr26@gmail.com>
2023-09-15 01:52:26 +1000

    Upcoming Pt. 2

--- main/src/pages/landing/Landing.tsx
+++ main/src/pages/landing/Landing.tsx
@@ -1,14 +1,14 @@
 import Home from './Home';
 import Offerings from './Offerings';
 import JoinClub from './JoinClub';
-// import Upcoming from './Upcoming';
+import Upcoming from './Upcoming';
 
 function Landing() {
 	return (
 		<>
 			<Home />
 			<Offerings />
-			{/* <Upcoming /> */}
+			<Upcoming />
 			<JoinClub />
 		</>
 	);
