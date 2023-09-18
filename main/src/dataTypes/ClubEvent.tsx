875b903166aee7448b46ca38753b0ba54dff2d76
Russal Arya <russalarya@gmail.com>
2023-09-18 15:20:14 +1000

    Updated event data type

--- main/src/dataTypes/ClubEvent.tsx
+++ main/src/dataTypes/ClubEvent.tsx
@@ -2,7 +2,7 @@ export type ClubEvent = {
     id: string;
     title: string;
     type: string;
-    shortDescription: string;
+    shortDescription?: string;
     year: number;
     date?: string;
     address?: string;
